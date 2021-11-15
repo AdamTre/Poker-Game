549d91a56b3284a522b21f50051d80737e6e1179
AdamTre <44305553+AdamTre@users.noreply.github.com>
2021-11-15 15:26:50 -0600

    Add files via upload

---
+++ Poker.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poker;
+
+/**
+ *
+ * @author charlotteolaniyi
+ */
+public class Poker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        StartPage s = new StartPage ();
+        
+        
+        s.setVisible(true);
+    }
+    
+}
